d861d7bcc21d4d05ba87c36b0b678e2ca9d2d242
Mark Fitzpatrick <markfitz815@gmail.com>
2018-08-02 21:15:53 +0100

    vue app created through vue cli and added a list to it

---
+++ addressApp/client/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li v-for="listItem in myList" >{{ listItem }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data: function() {
+    return {
+      myList: ['cat', 'dog', 'plz']
+    }
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
