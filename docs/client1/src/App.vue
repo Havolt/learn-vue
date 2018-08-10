27eb10b26f409ed1618c26b2879ecb4265d5c6b1
Mark Fitzpatrick <markfitz815@gmail.com>
2018-08-10 17:50:23 +0100

    set up cli vue with my own component

---
+++ docs/client1/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Message></Message>
+  </div>
+</template>
+
+<script>
+import Message from './components/Message.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Message
+  }
+}
+</script>
+
+<style>
+
+* {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+</style>
---
+++ docs/client1/src/components/Message.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Hello Vue</div>
+</template>
+
+<script>
+export default {
+  name: 'Message',
+  props: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
