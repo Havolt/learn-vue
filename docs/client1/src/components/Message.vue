bece837d831b83fe2279ef01ae4a33d1a0c4eb55
Mark Fitzpatrick <markfitz815@gmail.com>
2018-08-10 18:54:08 +0100

    added button counter and html to vue component/template

--- docs/client1/src/components/Message.vue
+++ docs/client1/src/components/Message.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>Hello Vue</div>
+  <div>
+    <div>Hello Vue</div><br>
+    <div>
+      <div>The counter has been clicked: {{ counter }} times</div>
+      <button @click="counter += 1">Increment counter</button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,6 +13,11 @@ export default {
   name: 'Message',
   props: {
 
+  },
+  data() {
+    return {
+      counter: 0
+    }
   }
 }
 </script>
